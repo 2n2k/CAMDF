7e14d9c0f96672eaf5779196f625c4409a651f96
2n2k <drexxxteris@gmail.com>
2024-01-01 20:58:33 +0200

    Create registration.js

---
+++ registration.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('registration')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_q6t1wy6';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
